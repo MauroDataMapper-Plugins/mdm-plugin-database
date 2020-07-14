a4dbe2c061623a52ddf21857ef47dd7fa482d06f
Adei Josol <helena.josol@bdi.ox.ac.uk>
2020-07-15 00:55:12 +0100

    Refactor main() method

--- src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugin/database/RemoteDatabaseDataModelImporterProviderService.groovy
+++ src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugin/database/RemoteDatabaseDataModelImporterProviderService.groovy
@@ -3,17 +3,15 @@ package uk.ac.ox.softeng.maurodatamapper.plugin.database
 import uk.ac.ox.softeng.maurodatamapper.util.Utils
 
 import org.apache.commons.cli.CommandLine
-import org.apache.commons.cli.CommandLineParser
 import org.apache.commons.cli.DefaultParser
 import org.apache.commons.cli.HelpFormatter
 import org.apache.commons.cli.Option
 import org.apache.commons.cli.OptionGroup
 import org.apache.commons.cli.Options
+import org.slf4j.LoggerFactory
 
 import ch.qos.logback.classic.LoggerContext
 import ch.qos.logback.core.util.StatusPrinter
-import org.slf4j.LoggerFactory
-
 import groovy.transform.CompileStatic
 
 import java.nio.file.Files
@@ -22,8 +20,6 @@ import java.nio.file.Paths
 
 @CompileStatic
 trait RemoteDatabaseDataModelImporterProviderService {
-
-    private static final CommandLineParser parser = new DefaultParser()
 
     private static Options defineOptions() {
         Collection<Option> optionsList = [
@@ -55,54 +51,52 @@ trait RemoteDatabaseDataModelImporterProviderService {
         options
     }
 
-    private static String version() {
-        "remote-database-importer " +
-        "  Version: \"${RemoteDatabaseDataModelImporterProviderService.getPackage().getSpecificationVersion()}\"\n" +
-        "  Java Version: \"${System.getProperty('java.version')}\""
+    private static void startService(CommandLine commandLine) {
+        Path path = Paths.get(commandLine.getOptionValue('c'))
+        println "Starting Remote Database Import service\n${getVersionInfo()}\nConfig file: ${path.toAbsolutePath()}\n"
+        Utils.outputRuntimeArgs(getClass())
+        new RemoteDatabaseImportAndExporter().performImportAndExport(
+            new Properties().with { Properties properties ->
+                load Files.newInputStream(path)
+                setProperty 'server.username', commandLine.getOptionValue('u')
+                setProperty 'server.password', commandLine.getOptionValue('p')
+                setProperty 'import.database.password', commandLine.getOptionValue('w')
+                properties
+            })
     }
 
-    private static void help() {
+    private static String getVersionInfo() {
+        new StringBuilder().with { StringBuilder stringBuilder ->
+            append 'remote-database-importer\n'
+            append "  Version: \"${RemoteDatabaseDataModelImporterProviderService.getPackage().getSpecificationVersion()}\"\n"
+            append "  Java version: \"${System.getProperty('java.version')}\""
+            stringBuilder.toString()
+        }
+    }
+
+    private static void printHelp() {
         new HelpFormatter().printHelp(
             120,
             'remote-database-importer -c <FILE> -u <USERNAME> -p <PASSWORD> -w <DATABASE_PASSWORD>',
             'Export database to Metadata Catalogue\nConnect to a database, export to DataModel and push to Metadata Catalogue server\n\n',
             defineOptions(),
-            "\n${version()}\n\nPlease report issues at https://metadatacatalogue.myjetbrains.com\n",
+            "\n${getVersionInfo()}\n\nPlease report issues at https://metadatacatalogue.myjetbrains.com\n",
             false)
     }
 
-    @SuppressWarnings('Println')
     static void main(String[] args) {
+        // Assume Slf4j is bound to Logback in the current environment
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory()
+        StatusPrinter.print loggerContext // Print Logback's internal status
 
-        // assume SLF4J is bound to logback in the current environment
-        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory()
-        // print logback's internal status
-        StatusPrinter.print(lc)
-
-        // parse the command line arguments
-        CommandLine line = parser.parse(defineOptions(), args)
-        if (line.hasOption('h')) help()
-        else if (line.hasOption('v')) println(version())
-        else if (line.hasOption('c') && line.hasOption('u') && line.hasOption('p')) {
-
-            Path path = Paths.get(line.getOptionValue('c'))
-
-            println('Starting Remote Database Import service\n' +
-                    "${version()}\n" +
-                    "  Config File: ${path.toAbsolutePath().toString()}\n")
-
-            RemoteDatabaseImportAndExporter remoteDatabaseImportAndExporter = new RemoteDatabaseImportAndExporter()
-
-            Utils.outputRuntimeArgs(getClass())
-
-            remoteDatabaseImportAndExporter.performImportAndExport(new Properties().with {
-                load(Files.newInputStream(path))
-                setProperty('server.username', line.getOptionValue('u'))
-                setProperty('server.password', line.getOptionValue('p'))
-                setProperty('import.database.password', line.getOptionValue('w'))
-                it
-            })
-
-        } else help()
+        // Parse command line arguments
+        CommandLine commandLine = new DefaultParser().parse(defineOptions(), args)
+        if ('cpu'.any { String option -> commandLine.hasOption option }) {
+            startService commandLine
+        } else if (commandLine.hasOption('v')) {
+            println getVersionInfo()
+        } else {
+            printHelp()
+        }
     }
 }
